1de170f1e58a41cf087adcf851c8940da070e6cb
Jake O'Toole <otoolej33@gmail.com>
2021-03-20 17:33:55 -0500

    Corrected morgan import into server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const morgan = rquire('morgan');
+const morgan = require('morgan');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
