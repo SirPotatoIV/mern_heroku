6ed287872f43397fc160ea0074baeba443e67882
Jake O'Toole <otoolej33@gmail.com>
2021-03-20 18:23:31 -0500

    Added post routes and added in basic operations to the MongoDB.

--- routes/api/pokemon.js
+++ routes/api/pokemon.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const db = require('../../models');
 
-router.route('/').get(function (req, res) {
-  res.json('allll the pokemon!');
-});
+router
+  .route('/')
+  .get(async function (req, res) {
+    const data = await db.Pokemon.find({});
+    res.json(data);
+  })
+  .post(async function (req, res) {
+    const data = await db.Pokemon.create(req.body);
+    res.json(data);
+  });
 
 module.exports = router;
--- routes/api/trainer.js
+++ routes/api/trainer.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const db = require('../../models');
 
-router.route('/').get(function (req, res) {
-  res.json('allll the trainers!');
-});
+router
+  .route('/')
+  .get(async function (req, res) {
+    const data = await db.Trainer.find({});
+    res.json(data);
+  })
+  .post(async function (req, res) {
+    const data = await db.Trainer.create(req.body);
+    res.json(data);
+  });
 
 module.exports = router;
