0483d60c51c91d63be586dd84283a248b766027e
Jake O'Toole <otoolej33@gmail.com>
2021-03-20 18:58:28 -0500

    Created very basic login form.

--- client/src/pages/PokeCenter.js
+++ client/src/pages/PokeCenter.js
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function PokeCenter() {
-  return <h2>PokeCenter</h2>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <div>
+      <h2>PokeCenter</h2>
+      <div>Welcome to the PokeCenter! Please check-in!</div>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="email">E-mail: </label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
