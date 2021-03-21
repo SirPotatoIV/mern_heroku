62d205cde33e9dc41c2ed2ee3a46fd7d2dfbc582
Jake O'Toole <otoolej33@gmail.com>
2021-03-20 21:03:44 -0500

    Added conditional to login and sign up components.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Login from '../components/Login';
 import SignUp from '../components/SignUp';
 
+import './Home.css';
+
 const Home = () => {
+  const [showLogin, setShowLogin] = useState(true);
+
   return (
     <>
-      <Login />
-      <SignUp />
+      {showLogin ? <Login /> : <SignUp />}
+      <button
+        className="loginSignup"
+        onClick={() => {
+          setShowLogin(!showLogin);
+        }}
+      >
+        {showLogin ? 'Go to Sign Up' : 'Go to Login'}
+      </button>
     </>
   );
 };
